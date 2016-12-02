0b05248ed61749aa590f7ab74a7bb4125c6af6cf
Simon Lagos <simon.lagos.sallhed@gmail.com>
2016-12-03 00:21:02 +0100

    Turn App into skeleton smart component

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react'
 import Header from './header'
+import Organization from './organization'
+import { connect } from 'react-redux'
 
-export default class App extends Component {
+export class App extends Component {
+
+  componentDidMount() {
+    this.props.loadInitialData()
+  }
+
   render(){
     return(
       <div>
         <Header />
+        <Organization />
       </div>
     )
   }
 }
+
+const mapStateToProps = state => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loadInitialData: () => {
+      
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
